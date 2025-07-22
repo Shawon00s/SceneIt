50d1a88c1a46cc04c08d4006b04870724e9d6395
Shawon00s <royshawonsudipto@gmail.com>
2025-07-23 01:25:49 +0600

    initialized the search UI

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,58 @@
+import MovieCard from '@/components/MovieCard';
+import SearchBar from '@/components/SearchBar';
+import { icons } from '@/constants/icons';
+import { images } from '@/constants/images'
+import { fetchMovies } from '@/services/api';
+import useFetch from '@/services/usefetch';
+import { useRouter } from 'expo-router';
 import React from 'react'
-import { Text, View } from 'react-native'
+import { FlatList, Image, Text, View } from 'react-native'
 
 const search = () => {
+  const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({
+    query: ""
+  }));
+
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200">Search</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className='flex-1 absolute w-full z-0' resizeMode='cover' />
+
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => (
+          <MovieCard {...item} />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        className='px-5'
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: 'center',
+          gap: 16,
+          marginBottom: 16,
+        }}
+        contentContainerStyle={{
+          paddingBottom: 100
+        }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+
+            <View className='my-5'>
+              <SearchBar placeholder='Search movies ...' />
+            </View>
+          </>
+      }
+    />
     </View>
-  )
+  );
 }
 
 export default search
