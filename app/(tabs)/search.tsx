403224993a9078ec8d08cc99ef156066c3fde71e
Shawon00s <royshawonsudipto@gmail.com>
2025-07-23 21:14:35 +0600

    updated search option and faced some issues

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,23 +1,23 @@
 import MovieCard from '@/components/MovieCard';
 import SearchBar from '@/components/SearchBar';
 import { icons } from '@/constants/icons';
-import { images } from '@/constants/images'
+import { images } from '@/constants/images';
 import { fetchMovies } from '@/services/api';
 import useFetch from '@/services/usefetch';
-import { useRouter } from 'expo-router';
-import React from 'react'
-import { FlatList, Image, Text, View } from 'react-native'
+import React, { useState } from 'react';
+import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native';
 
 const search = () => {
-  const router = useRouter();
+  //useState for search
+const [searchQuery, setSearchQuery] = useState("");
 
   const {
     data: movies,
-    loading: moviesLoading,
-    error: moviesError,
+    loading,
+    error,
   } = useFetch(() => fetchMovies({
-    query: ""
-  }));
+    query: searchQuery
+  }),false);
 
   return (
     <View className="flex-1 bg-primary">
@@ -46,11 +46,33 @@ const search = () => {
             </View>
 
             <View className='my-5'>
-              <SearchBar placeholder='Search movies ...' />
+              <SearchBar 
+                placeholder='Search movies ...'
+                value={searchQuery}
+                onChangeText={(text: string) => setSearchQuery(text)}
+              />
+
             </View>
+
+            {loading && (
+              <ActivityIndicator size="large" color="#0000ff" />
+            )}
+
+            {error && (
+              <Text className='text-red-500 px-5 my-3 text-center'>
+                {error.message}
+              </Text>
+            )}
+
+            {!loading && !error && searchQuery.trim() && movies && movies.length > 0 && (
+              <Text className='text-xl text-white font-bold'>
+                Search Results for {' '}
+                <Text className='text-accent'>{searchQuery}</Text>
+              </Text>
+            )}
           </>
-      }
-    />
+        }
+      />
     </View>
   );
 }
